7f80b6918843ce7d1aa524474eb4c29c04ad5ed7
mohamadkassemm <mohammaddkassemm@gmail.com>
2025-06-23 18:39:18 +0300

    facility and request routes

---
+++ backend/routes/facilitiesRoutes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const facilitiesController = require('../controllers/facilitiesController')
+
+router.post('/createShelter', facilitiesController.createShelter)
