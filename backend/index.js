ec054931bcba3307310380c04dc6a5d185904796
mohamadkassemm <mohammaddkassemm@gmail.com>
2025-07-01 12:12:09 +0300

    adding routes

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,12 @@ require('dotenv').config();
 DB();
 app.use(express.json())
 
+const facilityRoutes = require('./routes/facilitiesRoutes');
+const bookingRoutes = require('./routes/BookingRoutes')
+const offeringRoutes = require('./routes/offeringRoutes')
+
+app.use(`api/${process.env.API_VERSION}/shelter`, facilityRoutes)
+
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000")
 })
