a4b008047863516ad3e16e7cdfe473f5596ffb50
Mhamad mahdi <izcool337@gmail.com>
2025-07-06 00:06:58 +0300

    Revert "Added facilities by Chahine"

--- backend/routes/PersonRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const PersonController = require('../controllers/PersonControllers');
-
-router.post('/createPerson',PersonController.protect, PersonController.createPerson);
-router.put('/updatePerson/:id',PersonController.protect, PersonController.updatePerson);
-router.post('/login',PersonController.protect, PersonController.login)
-
-module.exports = router
--- backend/routes/requestRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const requestController = require('../controllers/requestsController')
-const PersonController = require('../controllers/PersonControllers')
-
-router.post('/createRequest',PersonController.protect, requestController.createRequest)
-module.exports = router
